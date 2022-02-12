44fb5d93f25082bb3a13dfae8f9fb6e21e9e46e2
Nikita <nikitazapletin@gmail.com>
2022-02-12 15:46:33 +0300

    menu active class and eventListner click on each elem-s add

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,17 @@ const items = document.querySelectorAll('.slider-item');
 const itemsCount = items.length;
 const itemWidth = container.clientWidth / slidesToShow;
 const movePosition = slidesToScroll * itemWidth;
+const menuItems = document.querySelectorAll('.header__link');
+
+
+menuItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        menuItems.forEach((items) => {
+            items.classList.remove('active');
+        });
+        item.classList.add('active');
+    });
+});
 
 
 items.forEach((item) => {

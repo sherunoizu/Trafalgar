a1cbd871597a314d98b7c7ab53de1aecf51a39bf
Nikita <nikitazapletin@gmail.com>
2022-02-18 10:45:17 +0300

    1\2 mobile adaptation

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,23 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      overlay = document.querySelector('.menu__overlay'),
+      closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => { 
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => { 
+    menu.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => { 
+    menu.classList.remove('active');
+});
+
+
+
+
 let position = 0; // Начальная позиция
 const slidesToShow = 1; // Сколько элементов показывать
 const slidesToScroll = 1; // Сколько элементов скроллить
@@ -10,16 +30,32 @@ const itemsCount = items.length;
 const itemWidth = container.clientWidth / slidesToShow;
 const movePosition = slidesToScroll * itemWidth;
 const menuItems = document.querySelectorAll('.header__link');
+const menuItemsDefault = document.querySelector('.header__link');
 
+
+window.addEventListener('scroll', refreshActiveMenu);
+
+
+
+function refreshActiveMenu() {
+    if (window.scrollY <= 50) {
+        activeDefault();
+        menuItemsDefault.classList.add('active');
+    }
+}
 
 menuItems.forEach((item) => {
     item.addEventListener('click', () => {
-        menuItems.forEach((items) => {
-            items.classList.remove('active');
-        });
+        activeDefault();
         item.classList.add('active');
     });
 });
+
+function activeDefault (){
+    menuItems.forEach((items) => {
+            items.classList.remove('active');
+        });
+}
 
 
 items.forEach((item) => {
@@ -55,3 +91,5 @@ const checkBtns = () => {
 };
 
 checkBtns();
+
+
